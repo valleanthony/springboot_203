3dbbd805d13cec7204f70e18ec62c05c81614acb
Anthony Valle <valleganthony@gmail.com>
2019-05-13 16:42:47 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_202/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.springboot_202;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/songform")
+    public String loadFormPage(Model model){
+        model.addAttribute("Song",new Song());
+        return "songform";
+    }
+
+    @PostMapping("/songform")
+    public String loadFormPage(@ModelAttribute Song song,Model model){
+        model.addAttribute("Song",song);
+        return "confirmsong";
+    }
+
+}
